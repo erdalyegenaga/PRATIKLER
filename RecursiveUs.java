636cc59f434560959c2fde492a38a77ce52f2a66
Erdal <69669049+erdalyegenaga@users.noreply.github.com>
2022-06-28 15:19:47 +0300

    Add files via upload

---
+++ RecursiveUs.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class RecursiveUs {
+    static  int usAlma(int taban, int us){
+        if(us==0) return 1;
+        return taban * usAlma(taban,us-1);
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Taban sayisini giriniz: ");
+        int taban = sc.nextInt();
+        System.out.print("Ussu giriniz: ");
+        int us = sc.nextInt();
+        System.out.println("Sonuc = " + usAlma(taban, us));
+    }
+}
